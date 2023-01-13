dcc44faa59b0874f8437a0a2c20452280f8f470d
ittp <61617660+ittp@users.noreply.github.com>
2023-01-13 03:32:40 +0300

    4 files modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import path from 'path';
+import axios from 'axios';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const Converter = require('csvtojson').Converter;
+
 root.render(
-  <>
+  <div>
+    <a href="1">1</a>
     <App title={'Convert'} />
-  </>
+  </div>
 );
